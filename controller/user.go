7eb3de57f14d51d6a727cf74b92619fcaaf0017f
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-04-09 19:34:53 +0530

    Add: CRUD implementation

--- controller/user.go
+++ controller/user.go
@@ -7,20 +7,33 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// controller to get the list of users
 func GetUsers(c *gin.Context) {
 	users := []models.User{}
 	config.DB.Find(&users)
 	c.JSON(200, &users)
 }
 
+// controller to create the user
 func CreateUser(c *gin.Context) {
-
+	var user models.User
+	c.BindJSON(&user)
+	config.DB.Create(&user)
+	c.JSON(200, &user)
 }
 
+// controller to delete the user
 func DeleteUser(c *gin.Context) {
-
+	var user models.User
+	config.DB.Where("id = ?", c.Param("id")).Delete(&user)
+	c.JSON(200, &user)
 }
 
+// controller to update the user
 func UpdateUser(c *gin.Context) {
-	
+	var user models.User
+	config.DB.Where("id = ?", c.Param("id")).First((&user))
+	c.BindJSON(&user)
+	config.DB.Save(&user)
+	c.JSON(200, &user)
 }
